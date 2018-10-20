a03bed000810e3147d83e06e4b333bd56e320610
manyuewuxin <996929178@qq.com>
2018-10-20 11:46:40 +0800

    修复Message

--- src/components/Header/User/MessageList.js
+++ src/components/Header/User/MessageList.js
@@ -12,44 +12,44 @@ export default class MessageList extends Component {
         scroll: PropTypes.func
     };
     render() {
-        const list = this.props.message.map((m) => {
-            const key = Math.random()
-                .toString(36)
-                .substring(2, 6);
-
-            if (m.message.message_type === "reply_posts") {
-                //优化合并，验证第三个参数arr本身
-                return (
-                    <li key={key}>
-                        <Link to={`people/${m.author[0]._id}/dynamic`}>
-                            {m.author[0].name}
-                        </Link>
-                        <span>评论了你的文章</span>
-                        <Link to={`/p/${m.posts[0]._id}`}>{m.posts[0].title}</Link>
-                    </li>
-                );
-            } else if (m.message.message_type === "reply_comment") {
-                return (
-                    <li key={key}>
-                        <Link to={`people/${m.author[0]._id}/dynamic`}>
-                            {m.author[0].name}
-                        </Link>
-                        <span>在文章</span>
-                        <Link to={`/p/${m.posts[0]._id}`}>{m.posts[0].title}</Link>
-                        <span>回复了你的评论</span>
-                    </li>
-                );
-            } else if (m.message.message_type === "follow") {
-                return (
-                    <li key={key}>
-                        <Link to={`people/${m.author[0]._id}/dynamic`}>
-                            {m.author[0].name}
-                        </Link>
-                        <span>关注了你</span>
-                    </li>
-                );
+        const { message } = this.props;
+        const list = [];
+        for(let i = 0; i < message.length; i++){
+            if(message[i].posts.length>0){
+                const key = Math.random().toString(36).substring(2, 6);
+                if (m.message.message_type === "reply_posts") {
+                    list.push(
+                        <li key={key}>
+                            <Link to={`people/${m.author[0]._id}/dynamic`}>
+                                {m.author[0].name}
+                            </Link>
+                            <span>评论了你的文章</span>
+                            <Link to={`/p/${m.posts[0]._id}`}>{m.posts[0].title}</Link>
+                        </li>
+                    );
+                } else if (m.message.message_type === "reply_comment") {
+                    list.push(
+                        <li key={key}>
+                            <Link to={`people/${m.author[0]._id}/dynamic`}>
+                                {m.author[0].name}
+                            </Link>
+                            <span>在文章</span>
+                            <Link to={`/p/${m.posts[0]._id}`}>{m.posts[0].title}</Link>
+                            <span>回复了你的评论</span>
+                        </li>
+                    );
+                } else if (m.message.message_type === "follow") {
+                    list.push(
+                        <li key={key}>
+                            <Link to={`people/${m.author[0]._id}/dynamic`}>
+                                {m.author[0].name}
+                            </Link>
+                            <span>关注了你</span>
+                        </li>
+                    );
+                }
             }
-        });
+        }
         return (
             <div style={{ position: "fixed", left: `${this.props.left}px`, zIndex: "4" }}>
                 <div className="show_message">
